eca8997232c58d0913d1d11270aab71a3a1df402
angadsingh <angad17@hotmail.com>
2025-10-10 09:34:49 +0100

    Remove SDK folder

--- sdk/lib/rewards.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * NOTE: Work in progress
- *
- * Rewards and Fee Collection Module
- * Handles reward distribution, fee collection, and reward management
- */
-//# sourceMappingURL=rewards.d.ts.map
