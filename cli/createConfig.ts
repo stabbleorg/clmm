7acb74ba61855dc288b91a3b93d4b06a666af175
angadsingh <angad17@hotmail.com>
2025-10-22 18:01:24 +0100

    fix createConfig CLI

--- cli/createConfig.ts
+++ cli/createConfig.ts
@@ -7,24 +7,26 @@ import {BN} from "@coral-xyz/anchor";
 export function createConfig(program: Command) {
   program
     .command("create-config")
-    .requiredOption("-i, --index <number>", "Index of the config (re-use will fail)", parseBN)
-    .requiredOption("-s, --tick-spacing <number>", "Tick spacing", parseBN)
-    .requiredOption("-t, --trade-fee-rate <number>", "Trade fee rate", parseBN)
-    .requiredOption("-p, --protocol-fee-rate <number>", "Protocol fee rate", parseBN)
-    .requiredOption("-f, --fund-fee-rate <number>", "Fund fee rate", parseBN)
+    .requiredOption("-i, --index <number>", "Index of the config (re-use will fail)", Number)
+    .requiredOption("-s, --tick-spacing <number>", "Tick spacing", Number)
+    .requiredOption("-t, --trade-fee-rate <number>", "Trade fee rate", Number)
+    .requiredOption("-p, --protocol-fee-rate <number>", "Protocol fee rate", Number)
+    .requiredOption("-f, --fund-fee-rate <number>", "Fund fee rate", Number)
     .description("Create a new AMM Config")
     .action(async ({ index, tickSpacing, tradeFeeRate, protocolFeeRate, fundFeeRate }:
       {
-        index: BN,
-        tickSpacing: BN,
-        tradeFeeRate: BN,
-        protocolFeeRate: BN,
-        fundFeeRate: BN,
+        index: number,
+        tickSpacing: number,
+        tradeFeeRate: number,
+        protocolFeeRate: number,
+        fundFeeRate: number,
       }) => {
+      const indexBuf = Buffer.alloc(2); // index is u16
+      indexBuf.writeUInt16BE(index); // big endian
       const [ammConfig] = PublicKey.findProgramAddressSync(
         [
           Buffer.from("amm_config"), // AMM_CONFIG_SEED
-          Buffer.from(new Uint8Array(index.toArray('be', 2))) // index.to_be_bytes()
+          indexBuf,
         ],
         clmmProgram.programId
       );
